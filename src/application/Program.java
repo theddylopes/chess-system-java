64890701bad1616f45a52c8589e1195e88cc7241
theddylopes <>
2021-02-03 22:21:52 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,29 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		Position p = new Position(3, 4);
 		
-		System.out.println(p);
+		Board board = new Board(8,8);
+		System.out.println(board.getPieces().length);
+		
+		
+		
+		for(int i = 0; i < board.getPieces().length; i++) 
+		{
+			for(int j = 0; j < board.getPieces()[i].length; j++)
+			{
+				
+				System.out.print(board.getPieces()[i][j] + " ");
+			
+			}
+			System.out.println("");
+		}
+		
 	}
-
+	
 }
